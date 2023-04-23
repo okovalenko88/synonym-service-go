f63145a81df6b48c8dadb01740e8e18594fc3a51
Oleksandr Kovalenko <okovalenko1988@gmail.com>
2023-04-23 20:45:07 +0200

    Get synonyms by key. Refactoring

--- synonym/synonym.go
+++ synonym/synonym.go
@@ -10,33 +10,31 @@ import (
 
 func Start() {
 	router := gin.Default()
-	router.GET("/synonyms", getSynonyms)
 	router.POST("/synonyms", postSynonyms)
+	router.GET("/synonyms", getSynonyms)
+	router.GET("/synonyms/:key", getSynonymsByKey)
 
 	router.Run("localhost:8080")
 }
 
-type synonyms struct {
-	Words []string `json:"words"`
-}
-
-// Store the synonyms into a map
-var synonymsMap = make(map[string][]string)
-
-// SafeMap is safe to use concurrently.
-type SafeMap struct {
-	mu sync.Mutex
-	sm map[string][]string
-}
-
-var safeSynonymsMap = SafeMap{sm: synonymsMap}
-
+// Handlers
 func getSynonyms(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, synonymsMap)
 }
 
+func getSynonymsByKey(c *gin.Context) {
+	key := c.Param("key")
+
+	if synonyms, exists := synonymsMap[key]; exists {
+		c.IndentedJSON(http.StatusOK, synonyms)
+		return
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "synonym not found"})
+}
+
 func postSynonyms(c *gin.Context) {
-	var newSynonyms synonyms
+	var newSynonyms synonymsPost
 
 	if err := c.BindJSON(&newSynonyms); err != nil {
 		return
@@ -47,6 +45,21 @@ func postSynonyms(c *gin.Context) {
 	updateSynonyms(words)
 }
 
+// Data structures
+type synonymsPost struct {
+	Words []string `json:"words"`
+}
+
+var synonymsMap = make(map[string][]string)
+
+type SafeMap struct {
+	mu sync.Mutex
+	sm map[string][]string
+}
+
+var safeSynonymsMap = SafeMap{sm: synonymsMap}
+
+// Functionality
 func updateSynonyms(words []string) {
 	key := words[0]
 	_, exists := synonymsMap[words[0]]
@@ -58,7 +71,7 @@ func updateSynonyms(words []string) {
 		var wordsCopy = make([]string, len(words))
 		copy(wordsCopy, words)
 		synonymWords := append(wordsCopy[0:i], wordsCopy[i+1:]...)
-		safeSynonymsMap.threadSafeMapInsert(word, synonymWords)
+		safeSynonymsMap.safeMapInsert(word, synonymWords)
 	}
 }
 
@@ -74,38 +87,11 @@ func removeDuplicates(lst []string) []string {
 	return list
 }
 
-func (sm *SafeMap) threadSafeMapInsert(key string, values []string) {
+/*
+ * Thread safe map insert
+ */
+func (sm *SafeMap) safeMapInsert(key string, values []string) {
 	sm.mu.Lock()
-	// sm.sm[key] = append(sm.sm[key], values...)
 	sm.sm[key] = values
 	sm.mu.Unlock()
-}
-
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
