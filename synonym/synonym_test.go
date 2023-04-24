dde2e29392649f94c909b46d78f2902208660dbd
Oleksandr Kovalenko <okovalenko1988@gmail.com>
2023-04-24 10:31:05 +0200

    Add test for removeDuplicates

---
+++ synonym/synonym_test.go
@@ -0,0 +1,15 @@
+package synonym
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	// test removeDuplicates
+
+	ans := removeDuplicates([]string{"a", "b", "a", "c", "b"})
+	if !reflect.DeepEqual(ans, []string{"a", "b", "c"}) {
+		t.Errorf("RemoveDuplicates failed")
+	}
+}
