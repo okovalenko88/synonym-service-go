cf1f38b994694e222dc432086ea10b899b770dfa
Oleksandr Kovalenko <okovalenko1988@gmail.com>
2023-04-24 11:10:55 +0200

    Remove comment

--- synonym/synonym_test.go
+++ synonym/synonym_test.go
@@ -17,8 +17,6 @@ func setup() {
 }
 
 func TestRemoveDuplicates(t *testing.T) {
-	// test removeDuplicates
-
 	ans := removeDuplicates([]string{"a", "b", "a", "c", "b"})
 	if !reflect.DeepEqual(ans, []string{"a", "b", "c"}) {
 		t.Errorf("RemoveDuplicates failed")
